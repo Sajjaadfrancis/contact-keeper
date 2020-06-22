cbcbf5ef27703362066bef244996a0eb93e9d55c
unknown <sajjaadfrancis@gmail.com>
2020-06-22 13:52:09 +0200

    finished project

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+// connect database
+connectDB();
+
+// init middelware
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the ContactKeeper API..." })
 );
 
 //define routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 const PORT = process.env.PORT || 5000;
 
